1865fcb2e899f04a7121fe1292c16454c9b55589
Tom Westerhout <t.westerhout@student.ru.nl>
2018-08-24 22:45:09 +0100

    Haskell examples

--- example/sr/Main.hs
+++ example/sr/Main.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE TypeFamilies     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE OverloadedStrings #-}
 --
 -- |
 -- Module      : Main
@@ -43,18 +44,22 @@ import           Data.Text                      ( Text )
 import           Data.Text.IO                   ( hGetLine )
 import qualified Data.Text.IO                  as T
 
+import           GHC.Float                      ( int2Float )
 
 import           Lens.Micro
 import           Lens.Micro.Extras
+import Data.Aeson
+import System.Log.FastLogger
 
 import           PSO.Random
 import           PSO.FromPython
 import           NQS.Rbm (Rbm, mkRbm)
 import           NQS.Rbm.Mutable -- (sampleGradients)
 import           NQS.Internal.Hamiltonian
-import           NQS.Internal.Types -- (ℂ, ℝ)
 import           NQS.Internal.Rbm (unsafeThawRbm)
-import           NQS.Internal.LAPACK
+import           NQS.Internal.Types -- (ℂ, ℝ)
+import           NQS.Internal.Sampling
+import           NQS.SR
 
 fromPyFile :: FilePath -> IO Rbm
 fromPyFile name = withFile name ReadMode toRbm
@@ -77,25 +82,34 @@ randomRbm n m (lowV, highV) (lowH, highH) (lowW, highW) = do
     visible <- uniformVector n (lowV :+ lowV, highV :+ highV)
     hidden  <- uniformVector m (lowH :+ lowH, highH :+ highH)
     weights <- uniformVector (n * m) (lowW :+ lowW, highW :+ highW)
-    return $ trace ("mkRbm...") (mkRbm visible hidden weights)
+    return $! mkRbm visible hidden weights
 
 numberSteps :: (Int, Int, Int) -> Int
 numberSteps (low, high, step) = (high - low - 1) `div` step + 1
 
-main = do -- NQS.Internal.LAPACK.test
-  let filename = "/home/tom/src/tcm-swarm/cbits/test/input/rbm_6_6_0.in"
-  rbm <- unsafeThawRbm =<< fromPyFile filename :: IO (MRbm (PrimState IO))
-  -- rbm <- unsafeThawRbm =<< randomRbm 100 200 (-0.1, 0.1) (-0.1, 0.1) (-0.05, 0.05)
+main = do
+  -- let filename = "/home/tom/src/tcm-swarm/cbits/test/input/rbm_6_6_0.in"
+  -- rbm <- unsafeThawRbm =<< fromPyFile filename :: IO (MRbm (PrimState IO))
+  rbm <- unsafeThawRbm =<< randomRbm 25 50 (-0.1, 0.1) (-0.1, 0.1) (-0.05, 0.05)
   let n = sizeVisible rbm
-      config = defaultMCConfig & steps .~ (1000, 21000 * n, n)
-                               & magnetisation .~ (Just 0)
+      mcConfig = defaultMCConfig & steps .~ (1000, 1000 + 10000 * n, n)
+                                 & magnetisation .~ (Just 1)
+                                 & restarts .~ 20
+      cgConfig = CGConfig 2000 1.0E-5
+      srConfig = SRConfig 500 (Just (\i -> (10.0 * 0.9 ^ i + 0.001) :+ 0))
+                              (\i -> (exp(- int2Float i / 10) + 0.03) :+ 0)
+                              cgConfig mcConfig
       nParams = size rbm
-      nSteps = numberSteps $ config ^. steps
-  print n
-  hamiltonian <- heisenberg (1, 1) (Just 5.0) (zip [0..] ([1..(n - 1)] ++ [0]))
+      nSteps = numberSteps $ mcConfig ^. steps
+  -- hamiltonian <- heisenberg (1, 1) (Just 5.0) $ (zip [0..] ([1..(n - 1)] ++ [0]))
+  hamiltonian <- heisenberg (1, 1) (Just 5.5) $
+    [(0,1),(0,5),(0,20),(0,4),(1,2),(1,6),(1,21),(2,3),(2,7),(2,22),(3,4),(3,8),(3,23),(4,9),(4,24),(5,6),(5,10),(5,9),(6,7),(6,11),(7,8),(7,12),(8,9),(8,13),(9,14),(10,11),(10,15),(10,14),(11,12),(11,16),(12,13),(12,17),(13,14),(13,18),(14,19),(15,16),(15,20),(15,19),(16,17),(16,21),(17,18),(17,22),(18,19),(18,23),(19,24),(20,21),(20,24),(21,22),(22,23),(23,24)]
+    -- [(0,1),(1,2),(2,0),(3,4),(4,5),(5,3),(6,7),(7,8),(8,6),(0,3),(3,6),(6,0),(1,4),(4,7),(7,1),(2,5),(5,8),(8,2)]
+  setLogLevel Tcm_LogInfo
+  withFastLogger (LogFileNoRotate "Main-Log.log" (4 * 4096)) $ \logger ->
+    sr srConfig hamiltonian rbm (logger . toLogStr . (<> "\n") . Data.Aeson.encode)
   moments <- MV.new 4
-  -- f <- newDenseVector nParams
-  -- grad <- newDenseMatrix (nSteps * config ^. runs) nParams
-  sampleMoments config hamiltonian rbm moments -- f grad
-  e <- MV.read moments 0
-  print ["Hello!", show e]
+  sampleMoments mcConfig hamiltonian rbm moments
+  moments' <- V.freeze moments
+  print moments'
+
